b2265ff1a3a6401a4e3f7a3c9d87c7faed7460e0
FAJO007 <syedshah11023@gmail.com>
2025-06-14 11:52:19 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 👇 this base must match your repo name
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: '/global-vehicle-info/'
 })
+
